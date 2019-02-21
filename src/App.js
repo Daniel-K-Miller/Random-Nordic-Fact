5628eaf7be5dd11ae4290672e6919c256875c0c7
Daniel-K-Miller <daniel_k_miller@hotmail.com>
2019-02-21 18:45:30 +0000

    Added an extended styled-component

--- src/App.js
+++ src/App.js
@@ -5,52 +5,77 @@ const Wrapper = styled.section`
   background: lightgrey;
   display: flex;
   justify-content: center;
+  flex-direction: column;
+`
+const InnerWrapper = styled.article`
+  display: block;
+  margin: 0 auto;
 `
 
-const Button = styled.button`
-  color: darkred;
+const Button = styled.a`
+  color: white;
   padding: .5em 1em;
   font-size: 1.2em;
-  background-color: white;
-  border: 2px solid darkred;
+  background: rosybrown;
+  border: 2px solid rosybrown;
   border-radius: 3px;
   text-align: center;
   display: inline-block;
   margin: 1em;
+  text-decoration: none;
   cursor: pointer;
-  ${props => props.primary && css`
+  ${props => props.styles === 'primary' && css`
   background: darkred;
   color: white;
   border: 2px solid darkred;
   `}
+  ${props => props.styles === 'secondary' && css`
+  background: forestgreen;
+  border: 2px solid forestgreen;
+  color: white;
+  `}
+`
+const ExtendedButton = styled(Button)`
+  background: khaki;
+  display: flex;
 `
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'Click here!'
+      text: 'Click here!',
+      styles: 'primary'
     }
   }
   handleClick = () => {
     this.setState({
-      text: "Clicked!"
+      text: "Clicked!",
+      styles: 'secondary'
     })
   }
   handleReset = () => {
     this.setState({
-      text: 'Click here!'
+      text: 'Click here!',
+      styles: 'primary'
     })
   }
   render() {
     return (
       <Wrapper>
-        <Button primary onClick={this.handleClick}>
-          {this.state.text}
-        </Button>
-        {this.state.text === "Clicked!" && <Button onClick={this.handleReset}>
-          Reset
+        <InnerWrapper>
+          <Button styles={this.state.styles} onClick={this.handleClick}>
+            {this.state.text}
+          </Button>
+          {this.state.text === "Clicked!" && <Button onClick={this.handleReset}>
+            Reset
         </Button>}
+        </InnerWrapper>
+        <InnerWrapper>
+          <ExtendedButton target="_blank" href="https://www.google.co.uk">
+            Testy
+          </ExtendedButton>
+        </InnerWrapper>
       </Wrapper>
     );
   }
