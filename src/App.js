9cb34843e02bc8a82c284ab0239ca872c3e3513a
Daniel-K-Miller <daniel_k_miller@hotmail.com>
2019-02-21 16:20:33 +0000

    added a simple two button component to test basic styled-component syntax

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import React, { Component } from 'react';
-import styled, { CSS } from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Wrapper = styled.section`
   background: lightgrey;
+  display: flex;
+  justify-content: center;
 `
 
 const Button = styled.button`
-  color: ${props => props.primary ? "white" : "darkred"};
-  padding: 1rem;
-  font-size: 1em;
-  background-color: ${props => props.primary ? "darkred" : "transparent"};
-  border: 2px solid black;
+  color: darkred;
+  padding: .5em 1em;
+  font-size: 1.2em;
+  background-color: white;
+  border: 2px solid darkred;
   border-radius: 3px;
   text-align: center;
-  display: block;
-  margin: 0 auto;
+  display: inline-block;
+  margin: 1em;
+  cursor: pointer;
+  ${props => props.primary && css`
+  background: darkred;
+  color: white;
+  border: 2px solid darkred;
+  `}
 `
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Click here!'
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      text: "Clicked!"
+    })
+  }
+  handleReset = () => {
+    this.setState({
+      text: 'Click here!'
+    })
+  }
   render() {
     return (
       <Wrapper>
-        <Button>
-          Click here!
+        <Button primary onClick={this.handleClick}>
+          {this.state.text}
         </Button>
-        <Button primary>
-          Click here!
-        </Button>
+        {this.state.text === "Clicked!" && <Button onClick={this.handleReset}>
+          Reset
+        </Button>}
       </Wrapper>
     );
   }
