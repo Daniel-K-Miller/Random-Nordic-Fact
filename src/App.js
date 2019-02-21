f600ad3856529de42754bd2d59446df69eca4958
Daniel Kieron Miller <daniel_k_miller@hotmail.com>
2019-02-21 14:06:29 +0000

    initial commit, just a test area for styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { CSS } from 'styled-components';
+
+const Wrapper = styled.section`
+  background: lightgrey;
+`
+
+const Button = styled.button`
+  color: ${props => props.primary ? "white" : "darkred"};
+  padding: 1rem;
+  font-size: 1em;
+  background-color: ${props => props.primary ? "darkred" : "transparent"};
+  border: 2px solid black;
+  border-radius: 3px;
+  text-align: center;
+  display: block;
+  margin: 0 auto;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Button>
+          Click here!
+        </Button>
+        <Button primary>
+          Click here!
+        </Button>
+      </Wrapper>
     );
   }
 }
