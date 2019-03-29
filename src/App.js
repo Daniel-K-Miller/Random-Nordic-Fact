7ba901eb7d02ad427ef3bdfbfb57ae0667a1faf2
Daniel Kieron Miller <daniel_k_miller@hotmail.com>
2019-03-29 00:09:59 +0000

    Added more comprehensive documentation within App.js

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ class App extends Component {
       fact: '',
       // text to display whether fact is: current, previous, or andepenultimate fact, or one before that
       posOfFact: 'Current Fact',
+      // List of Fact Arrays, this is where they begin before they are copied and manipulated within different methods.
       finlandFacts: [`Finland joined the European Union in 1995.`, `The company Nokia got its name from the Town in Finland.`, `Nokia was founded in 1865 as a pulp mill and later transitioned onto electronics.`, `Finland was ranked as the #1 happiest place to live in the world according to the UN’s World Happiest Report 2018.`, `Speeding tickets in Finland are based on total income of the violator instead of a universal set fee.`, `Finland adopted the Euro on 1st January 1999 and abandoned the Finnish Markka on 28th February 2002.`, `The average Finn consumes 12 Kilos of coffee each year.`, `Finland produces the greatest number of heavy-metal bands per capita.`, `In 2006 Finland won Eurovision song contest with the band Lordi performing the song ‘hard rock hallelujah’.`, `Finland has come last in the Eurovision Song Contest 10 times (1963, 1965, 1968, 1980, 1982, 1990, 1992, 1996, 2009, 2015).`, `The population of Finland in 2017 was 5.5 million.`, `It is estimated that there are 2 million saunas in Finland.`, `Finnish is a Uralic language with Estonian being the closest related language.`, `In 1906, Finland was the first European country to allow all women to vote.`, `Finland elected its first female prime minister (Anneli Jäätteenmäki) in April 2003, making it the only country in Europe with both a female president (Tarja Halonen) and prime minister.`, `The operating system Linux was created by Finn Linus Torvalds in 1991. Linus also created the version control system Git in 2005.`, `Finn Kimi Räikkönnen won the 2007 F1 World Championship with 110 points.`, `The capital of Finland is Helsinki with a population of 631,695 in 2017.`, `Finland’s national animal is the brown bear`],
       swedenFacts: [`Sweden joined the European Union in 1995.`, `Sweden is owed approximately €254,000,000 by North Korea. This is due to Kim Il-sung striking a deal to buy 1000 Volvo cars from the Swedish government in 1974 and not paying for the goods.`, `Sweden is the fifth biggest country in Europe (447,435 km2m), and also has the second lowest population per square kilometre in Europe.`, `Sweden has won the Eurovision Song Contest 6 times (1974, 1984, 1991, 1991, 2012, 2015) making it the second highest winning country.`, `Sweden has come last in the Eurovision song contest 2 times (1963, 1977).`, `Sweden has produced multiple platinum selling artists. The most notable being: ABBA (375 million album/single sales), Roxette (75 million album/single sales), Ace of Base (50 million album/single sales), Avicii (30 million album/single sales), Europe (23 million album/single sales).`, `Sweden has the greatest number of McDonald’s restaurants per capita.`, `Sweden is the only country in which donations make up more than 1% of the country’s GDP.`, `The Swedish music group ABBA had to negotiate naming rights with the Abba Seafood Company which was founded in 1838.`, `The population of Sweden was 9.9 million in 2017.`, `The currency used in Sweden is Swedish crowns (SEK).`, `The capital of Sweden is Stockholm with a population of 960,031 in 2017.`, `Swede Daniel Ek founded the streaming service Spotify in 2008.`, `Three Swedish companies earned a spot in Fortunes Magazines Global 500 in 2017. Volvo (301), Ericsson (419), and H&M (482).`, `The Nobel Prize award is named after Swedish scientist Alfred Nobel. Nobel’s most famous invention was dynamite.`, `Sweden has not participated in any war for almost two centuries.`, `Swedish parents are entitled to 480 days of paid parental leave—and of those, 60 days are reserved for the father. In 2012, dads used 24% of the total parental leave.`, `Sweden’s national animal is the Eurasian Elk`],
       norwayFacts: [`Norway is not a member of the European Union.`, `In 1994 Norway held a referendum on whether to join the EU, the ‘no’ side won with 52.2% of the vote.`, `The currency in Norway is the Norwegian Krone.`, `The population of Norway in 2017 was 5.2 million.`, `The capital of Norway is Oslo with a population of 634,293 in 2017.`, `Europe’s northernmost point is North Cape within the Municipality of Nordkapp.`, `Norwegian Johan Vaaler has been credited as the inventor of the paper clip.`, `The Norwegian Government Pension Fund, also known as the ‘oil fund’ has over $1 trillion in assets. It is worth $195,000 per Norwegian citizen.`, `Norwegian pop group A-ha are named after jottings down in a notebook. Lead singer, Morten Harket noticed the word “aha” in fellow bandmate Paul Waaktaar-Savoy’s notebook and this led to the band’s name.`, `The Norwegian oil company Equinor is the only Norwegian company to earn a spot in Fortunes Magazines Global 500 in 2017 ranking at 207.`, `The world’s largest road tunnel called Lærdal Tunnel resides in Norway measuring 24.5Km long.`, `All residents of Norway have three figures published about their wealth, these include: their annual income, income tax paid, and total wealth. Pre-2013 this data was completely open, now a person instead has to request information of another and that person is able to see who has looked up their data regarding these figures.`, `Ancient and modern skiing were invented in Norway. Norwegian Sondre Norheim is seen as the father of modern skiing.`, `Norway has won the Eurovision Song Contest 3 times (1985, 1995, 2009).`, `Norway has come last in the Eurovision Song Contest 11 times (1963, 1969, 1974, 1976, 1978, 1981, 1990, 1997, 2001, 2004, 2012).`, `Norway’s national animal is the lion.`],
@@ -82,6 +83,10 @@ class App extends Component {
   }
 
   componentDidMount() {
+    // Reads originArray through a forEach function with a regex condition to test if each array item starts with lowerCase or upperCase
+    // Each item is then put into a new Object as a key with a value of 1; signifying that each key is currently active.
+    // (Information used for Navbar toggles within Navi.js -> List.js)
+
     // variable used to setState at end of componentWillMount()
     let tempObj = {};
     originArray.forEach(function eachKey(key) {
@@ -110,6 +115,8 @@ class App extends Component {
     })
     // use tempObj to finally set the state
     this.setState({ objectFromArray: tempObj })
+
+    // Creates a copy of the original fact arrays so when an array is emptied in the state through state changes it can be reset by these initial loaded arrays
     fullFinlandFacts = [...this.state.finlandFacts];
     fullSwedenFacts = [...this.state.swedenFacts];
     fullNorwayFacts = [...this.state.norwayFacts];
@@ -118,19 +125,25 @@ class App extends Component {
   }
 
   handleClick = () => {
-    // An array created consisting of the past 3 country's name. Used by PastFacts Component to map the past 3 facts. Array starts empty then fills over the past 3 countries names.
+    // handleClick event used by primButton.js within Buttons.js
+    /* Two arrays created consisting of the past 3 country's name and past 3 country's facts */
+    // This array starts empty then fills over the past 3 countries names.
     let prevState = dynamicArray[this.state.index]
     let prevFact = this.state.fact.slice(0);
-    if (prevState !== undefined) {
+    // First click will always have the prevState as undefined as code block has not finished executing, but after first iteration it will push value into array
+    if (prevState) {
       prevStateArray.unshift(prevState)
       prevFactArray.unshift(prevFact);
     }
+    // Only store 3 values in the two prevState, prevFact arrays so if it get larger than 3 values remove the oldest value.
     if (prevStateArray.length > 3) {
       prevStateArray.pop();
       prevFactArray.pop();
     }
+    // Finally set the prev... arrays within state based off the updates variables.
     this.setState({ lastCountries: prevStateArray, lastFacts: prevFactArray });
 
+    /* Dyanamic array created based on whether objectFromArray's item values are 1 or 0. If 1 they are added to this array (signifying they're active) */
     // Creates copy of objectFromArray that is in state
     let tempObj = { ...this.state.objectFromArray }
     // Iterate over each key within tempObj
@@ -147,6 +160,7 @@ class App extends Component {
     })
 
 
+    /* Random selection is made based off items that exist witin dynamicArray */
 
     // Expression used to create an index to be used with dynamicArray to select a random item
     let random = Math.floor(Math.random() * dynamicArray.length);
@@ -160,15 +174,13 @@ class App extends Component {
 
     this.setState({ country: tempCountry })
 
-    // variable to be used to gets all of the state to then be called to look for certain state key
+    /* variable to be used to gets all of the state to then be called to look for certain state key, auto-locate fact arrays / get random fact */
     let tempState = { ...this.state };
-    // variable to get the next country name that is passed down
 
     // putting the variable to start with lowerCase so it can be matched against a state key
     if (tempCountry) {
-
       tempCountry = tempCountry.charAt(0).toLowerCase() + tempCountry.slice(1)
-      // blank array that will be filled
+      // blank array that will be filled will country's facts in next part
       let array = []
       // iterate through the copy of this.state until a name copy is found e.g. tempCountry === "finland" and state has key called "finlandFacts"
       Object.keys(tempState).find(function (key) {
@@ -178,11 +190,12 @@ class App extends Component {
         }
         return undefined
       })
-      // randomly select an item from the area getting a random quote
+      // randomly select an item from the array getting a random quote
       let index = Math.floor(Math.random() * array.length);
-      // set the State based off the random country passed down from App.js and selectected random quote from that country's array
+      // setting the random fact based on the above factors. 
+      // ERROR SHOULD NOT TRIGGER, handleArray() MAKES SURE A SINGLE ITEM HAS TO BE ALWAYS TRUTHY in ObjectFromArray!
       array.length > 0 ? this.setState({ fact: array[index] }) : this.setState({ fact: "Error" })
-      // remove the quote from temp array above as the fact has been set now
+      // remove the quote from array above as the fact has been set now and do not want it to picked again until entire array has reset.
       array.splice(index, 1);
       // if statements based on nextProps.country's name
       if (tempCountry === "iceland") {
@@ -202,6 +215,7 @@ class App extends Component {
     }
   }
 
+  // Used within Buttons.js -> ResetButton.js
   handleReset = () => {
     this.setState({
       clicked: false,
@@ -210,10 +224,10 @@ class App extends Component {
     })
   }
 
+  // Used within Navi.js -> list.js
   handleArray = (element) => {
-    // Changes state of clicked so within facts.js with a method the facts are not re-evaluated
-    this.setState({ clicked: false })
-    // tempObj copy of objectFromArray in state, used again at the end of this method
+
+    // tempObj copy of objectFromArray in state, used here to toggle values to 0/1 by clicking buttons within the Navi.js component
     let tempObj = { ...this.state.objectFromArray };
 
 
@@ -232,11 +246,7 @@ class App extends Component {
       }
     })
     // finally set the state using the tempObj variable at the start of the method
-
     this.setState({ objectFromArray: tempObj })
-
-    // PREVENT LIST FROM BEING ONLY ONE ITEM! BASED OFF THIS VARIABLE
-
   }
 
   handleMouseOver = (index) => {
