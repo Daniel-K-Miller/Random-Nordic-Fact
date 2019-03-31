c661cccca28f9834e3db5c6862ffd19334b332da
Daniel Kieron Miller <daniel_k_miller@hotmail.com>
2019-03-31 18:14:21 +0100

    Updates

---
+++ precache-manifest.c01f59c0e8a80e2ba528d611a5747bc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "acbedca4174a022e4911",
+    "url": "/static/js/main.96b6e5eb.chunk.js"
+  },
+  {
+    "revision": "79fe81103ef4459ce69f",
+    "url": "/static/js/2.29996ec3.chunk.js"
+  },
+  {
+    "revision": "acbedca4174a022e4911",
+    "url": "/static/css/main.f4d76f64.chunk.css"
+  },
+  {
+    "revision": "66aa1d353139ab092aa6c8fc74932d53",
+    "url": "/index.html"
+  }
+];
